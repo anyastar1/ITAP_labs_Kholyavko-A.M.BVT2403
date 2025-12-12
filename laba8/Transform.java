6ca3c65b0a30be5ebd4bb7a5e07f70421387e85d
Anna Kholyavko <miss.kholyavko2006@gmail.com>
2025-12-12 17:19:15 +0300

    Transform.java

--- laba8/Transform.java
+++ laba8/Transform.java
@@ -2,7 +2,7 @@ import java.util. List;
 import java.util.stream. Collectors;
 import java.util. stream.Stream;
 
-public class TransformProcessor {
+public class Transform {
 
     @DataProcessor(description = "Преобразует строки в верхний регистр", priority = 2)
     public List<String> transformToUpperCase(Stream<String> dataStream) {
